5a26b4cd644612993b4b7ebe48ab46a4ee5b2adb
levietanh0108 <levietanhzz108@gmail.com>
2023-11-05 23:54:39 +0700

    xu ly tien te BestDealsApdater

---
+++ app/src/main/java/com/example/ungdungbanhang/adapters/BestDealsApdater.kt
@@ -0,0 +1,67 @@
+package com.example.ungdungbanhang.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.bumptech.glide.Glide
+import com.example.ungdungbanhang.data.Product
+import com.example.ungdungbanhang.databinding.BestDealsRvItemBinding
+import java.text.NumberFormat
+import java.util.Currency
+
+class BestDealsApdater: RecyclerView.Adapter<BestDealsApdater.BestDealsViewHolder>(){
+
+    inner class BestDealsViewHolder( private val binding: BestDealsRvItemBinding): ViewHolder(binding.root){
+        fun bind(product: Product){
+            binding.apply {
+                Glide.with(itemView).load(product.images[0]).into(imgBestDeal)
+                product.offerPercentage?.let {
+                    val remainingPricePercentage = 1f  - it
+                    val priceAffterOffer = remainingPricePercentage * product.price
+                    //tvNewPrice.text = "$ ${String.format("%.2f",priceAffterOffer)}"
+                    tvNewPrice.text = formatTienTeVietNam(priceAffterOffer.toDouble())
+                }
+                tvOldPrice.text = "$ ${product.price}"
+                tvDealProductName.text = product.name
+            }
+        }
+    }
+    private val diffCallback = object : DiffUtil.ItemCallback<Product>(){
+        override fun areItemsTheSame(oldItem: Product, newItem: Product): Boolean {
+            return  oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Product, newItem: Product): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    val differ = AsyncListDiffer(this,diffCallback)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BestDealsViewHolder {
+        return BestDealsViewHolder(
+            BestDealsRvItemBinding.inflate(
+                LayoutInflater.from(parent.context)
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: BestDealsViewHolder, position: Int) {
+        val product = differ.currentList[position]
+        holder.bind(product)
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    // doi tien te
+  fun formatTienTeVietNam(giaTienTrongVND: Double): String {
+        val numberFormat = NumberFormat.getCurrencyInstance()
+        numberFormat.currency = Currency.getInstance("VND")
+        return numberFormat.format(giaTienTrongVND)
+    }
+}
