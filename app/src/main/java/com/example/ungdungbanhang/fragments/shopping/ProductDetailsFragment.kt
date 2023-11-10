bdfd1079886d6e9b1c7abffbe499eecfe99d2508
levietanh0108 <levietanhzz108@gmail.com>
2023-11-10 22:04:36 +0700

    Dieu huong productDetailFragment

--- app/src/main/java/com/example/ungdungbanhang/fragments/shopping/ProductDetailsFragment.kt
+++ app/src/main/java/com/example/ungdungbanhang/fragments/shopping/ProductDetailsFragment.kt
@@ -1,4 +1,33 @@
 package com.example.ungdungbanhang.fragments.shopping
 
-class ProductDetailsFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.example.ungdungbanhang.adapters.ColorsAdapter
+import com.example.ungdungbanhang.adapters.SizesAdapter
+import com.example.ungdungbanhang.adapters.ViewPager2Images
+import com.example.ungdungbanhang.databinding.FragmentProductDetailsBinding
+
+class ProductDetailsFragment: Fragment() {
+    private val args by navArgs<ProductDetailsFragmentArgs>()
+    private lateinit var binding: FragmentProductDetailsBinding
+    private val viewPagerAdapter by lazy { ViewPager2Images() }
+    private val sizeApdater by lazy { SizesAdapter() }
+    private val colorAdapter by lazy { ColorsAdapter() }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentProductDetailsBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
